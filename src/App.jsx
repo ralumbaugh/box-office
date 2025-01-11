34299de6c970938b1f7dad9a48161688db9095b6
Ryan Alumbaugh <ralumbaugh@gmail.com>
2025-01-10 21:09:45 -0800

    Added router. Added home page. Added starred page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home";
+import Starred from "./Pages/Starred";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/starred" element={<Starred />}></Route>
+        <Route
+          path="/*"
+          element={<div>I don't know man, I'm scared!</div>}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
