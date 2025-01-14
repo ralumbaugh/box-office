9cdce91c9702ab327c8387db0af9300773591f27
Ryan Alumbaugh <ralumbaugh@gmail.com>
2025-01-13 16:42:14 -0800

    Installed react-query. Replaced useState with usequery hook. Re-enabled strict mode.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./Pages/Home";
 import Starred from "./Pages/Starred";
 import MainLayout from "./Components/MainLayout";
 import ShowPage from "./Pages/Show";
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/starred" element={<Starred />} />
-        </Route>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/starred" element={<Starred />} />
+          </Route>
 
-        <Route path="/show/:showId" element={<ShowPage />} />
-        <Route
-          path="/*"
-          element={<div>I don't know man, I'm scared!</div>}
-        ></Route>
-      </Routes>
-    </BrowserRouter>
+          <Route path="/show/:showId" element={<ShowPage />} />
+          <Route
+            path="/*"
+            element={<div>I don't know man, I'm scared!</div>}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
