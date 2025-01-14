932a563efe64f1cd514a749fdba250f00368b8a2
Ryan Alumbaugh <ralumbaugh@gmail.com>
2025-01-13 16:08:33 -0800

    Added individual show page. Disabled react strict mode.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./Pages/Home";
 import Starred from "./Pages/Starred";
 import MainLayout from "./Components/MainLayout";
+import ShowPage from "./Pages/Show";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/starred" element={<Starred />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/starred" element={<Starred />} />
         </Route>
+
+        <Route path="/show/:showId" element={<ShowPage />} />
         <Route
           path="/*"
           element={<div>I don't know man, I'm scared!</div>}
--- src/utils/tvmaze.js
+++ src/utils/tvmaze.js
@@ -8,4 +8,7 @@ const apiGet = async (queryString) => {
 };
 
 export const searchForShows = (query) => apiGet(`/search/shows?q=${query}`);
+
 export const searchForActors = (query) => apiGet(`/search/people?q=${query}`);
+
+export const getShowById = (showId) => apiGet(`/shows/${showId}`);
